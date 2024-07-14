e5c10b412a11e8ec7c784ae13baa7e2a998836ac
Daniel Hovach <daniel.hovach@gmail.com>
2024-07-14 09:11:20 +0200

    Create objects backup | Refactory collection move method

--- __init__.py
+++ __init__.py
@@ -6,12 +6,12 @@ import os
 bl_info = {
     "name": "Save as Backup",
     "author": "https://github.com/maqq1e",
-    "description": "Easy and fast save sepearet objects and collection as backup .blend files",
+    "description": "Easy and fast save separate objects and collection as backup .blend files",
     "blender": (4, 0, 0),
-    "version": (0, 1, 0),
+    "version": (1, 0, 0),
 }
 
-def create_unique_path(collection, directory):
+def create_unique_path(name, directory):
     
     isFileExist = True
 
@@ -20,7 +20,7 @@ def create_unique_path(collection, directory):
     while isFileExist:
         
         # Set the filename for the backup
-        backup_filename = f"Backups/{collection.name} {number}.blend"
+        backup_filename = f"Backups/{name} {number}.blend"
 
         backup_filepath = os.path.join(directory, backup_filename)
 
@@ -30,40 +30,30 @@ def create_unique_path(collection, directory):
         else:
             return backup_filepath
         
-
-def move_objects_from_collection(collection_from, collection_to):
-    # Ensure the collection exists
-    if collection_from not in bpy.data.collections:
-        print(f"Collection '{collection_from}' does not exist.")
-        return
-    
-    # Ensure the target collection exists, if not, create it
-    if collection_to not in bpy.data.collections:
-        target_collection = bpy.data.collections.new(collection_to)
+def move_objects_to_collection(objects, target_collection_name):
+    # Ensure the target collection exists
+    if target_collection_name not in bpy.data.collections:
+        target_collection = bpy.data.collections.new(target_collection_name)
         bpy.context.scene.collection.children.link(target_collection)
     else:
-        target_collection = bpy.data.collections[collection_to]
-    
-    # Get the collection to delete
-    collection_to_delete = bpy.data.collections[collection_from]
-    
-    # Move all objects to the target collection
-    for obj in collection_to_delete.objects:
+        target_collection = bpy.data.collections[target_collection_name]
+
+    # Iterate through all objects in the scene
+    for obj in objects:
+        # Skip objects already in the target collection
+        if target_collection_name in [col.name for col in obj.users_collection]:
+            continue
+
+        # Remove the object from all other collections
+        for col in obj.users_collection:
+            col.objects.unlink(obj)
+        
+        # Add the object to the target collection
         target_collection.objects.link(obj)
     
-    # Unlink the objects from the collection to delete
-    for obj in collection_to_delete.objects:
-        collection_to_delete.objects.unlink(obj)
-    
-    # Delete the collection
-    bpy.data.collections.remove(collection_to_delete)
-
     return target_collection
 
-
-
-def save_collection_backup(context, collection):
-
+def save_backup_WRAP(context, func, data):
     bpy.ops.wm.save_mainfile()
 
     # Get the current blend file path
@@ -71,21 +61,29 @@ def save_collection_backup(context, collection):
     if not blend_filepath:
         print("Save the .blend file first.")
         return
-    
+
     # Get the directory of the current blend file
     directory = os.path.dirname(blend_filepath)
-
     
+    # Create path
     if not os.path.exists(directory + "\Backups"):
         os.makedirs(directory + "\Backups")
 
+    backup_filepath = func(context, data, directory)    
 
-    backup_filepath = create_unique_path(collection, directory)
-        
+    # Create a new blend file for the backup
+    bpy.ops.wm.save_as_mainfile(filepath=backup_filepath, copy=True)
+    
+    # Reopen the original blend file
+    bpy.ops.wm.open_mainfile(filepath=blend_filepath)
+
+def save_collection_backup(context, collection, directory):
+
+    unique_path = create_unique_path(collection.name, directory)
 
     new_collection_name = 'Backup - ' + collection.name
-
-    new_collection = move_objects_from_collection(collection.name, new_collection_name)
+        
+    new_collection = move_objects_to_collection(collection.objects, new_collection_name)
 
     # Deselect all collections
     for coll in bpy.data.collections:
@@ -98,12 +96,30 @@ def save_collection_backup(context, collection):
     for coll in bpy.data.collections:
         if coll != new_collection:
             bpy.data.collections.remove(coll)
+
+    return unique_path
+
+def save_selected_objects_backup(context, objects, directory):
+
+    unique_path = create_unique_path(objects[0].name, directory)
+
+    new_collection_name = 'Backup - ' + objects[0].name
+
+    new_collection = move_objects_to_collection(objects, new_collection_name)
+
+    # Deselect all collections
+    for coll in bpy.data.collections:
+        coll.hide_viewport = True
+
+    # Select only the target collection
+    new_collection.hide_viewport = False
     
-    # Create a new blend file for the backup
-    bpy.ops.wm.save_as_mainfile(filepath=backup_filepath, copy=True)
-    
-    # Reopen the original blend file
-    bpy.ops.wm.open_mainfile(filepath=blend_filepath)
+    # Remove all collections except the selected one
+    for coll in bpy.data.collections:
+        if coll != new_collection:
+            bpy.data.collections.remove(coll)
+
+    return unique_path
 
 # Operator for saving collection backup
 class OBJECT_OT_save_collection_backup(bpy.types.Operator):
@@ -116,7 +132,7 @@ class OBJECT_OT_save_collection_backup(bpy.types.Operator):
         if bpy.data.is_saved:
             collection = bpy.data.collections.get(self.collection_name)
             if collection:
-                save_collection_backup(context, collection)
+                save_backup_WRAP(context, save_collection_backup, collection)
                 self.report({'INFO'}, f"Backup saved.")
             else:
                 self.report({'ERROR'}, f"Collection not found")
@@ -125,24 +141,49 @@ class OBJECT_OT_save_collection_backup(bpy.types.Operator):
             self.report({'ERROR'}, f"You need to save you .blend file!")
             return {'FINISHED'}
 
+# Operator for saving selected objects backup
+class OBJECT_OT_save_selected_objects_backup(bpy.types.Operator):
+    bl_idname = "object.save_selected_objects_backup"
+    bl_label = "Save Backup"
+    
+    def execute(self, context):
+        if bpy.data.is_saved:
+            if context.selected_objects:
+                save_backup_WRAP(context, save_selected_objects_backup, context.selected_objects)
+                self.report({'INFO'}, "Backup saved for selected objects")
+            else:
+                self.report({'ERROR'}, "No objects selected")
+            return {'FINISHED'}
+        else:
+            self.report({'ERROR'}, f"You need to save you .blend file!")
+            return {'FINISHED'}
+
 
 # Menu entry for the context menu
-def menu_func(self, context):
+def collection_func(self, context):
     layout = self.layout
     selected_collection = context.collection
     if selected_collection:
         layout.operator(OBJECT_OT_save_collection_backup.bl_idname, text="Save Backup").collection_name = selected_collection.name
 
-    layout.operator(OBJECT_OT_save_selected_objects_backup.bl_idname, text="Save Backup")
+def object_func(self, context):
+    layout = self.layout
+    selected_object = context.selected_objects
+    
+    layout.operator(OBJECT_OT_save_selected_objects_backup.bl_idname, text="Save Backup").selected_objects = selected_object
 
 # Registering the operator and menu
 def register():
     bpy.utils.register_class(OBJECT_OT_save_collection_backup)
-    bpy.types.OUTLINER_MT_collection.append(menu_func)
+    bpy.utils.register_class(OBJECT_OT_save_selected_objects_backup)
+    bpy.types.OUTLINER_MT_collection.append(collection_func)
+    bpy.types.OUTLINER_MT_object.append(object_func)
 
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_save_collection_backup)
-    bpy.types.OUTLINER_MT_collection.remove(menu_func)
+    bpy.utils.unregister_class(OBJECT_OT_save_selected_objects_backup)
+    bpy.types.OUTLINER_MT_collection.remove(collection_func)
+    bpy.types.OUTLINER_MT_object.remove(object_func)
 
 if __name__ == "__main__":
     register()
